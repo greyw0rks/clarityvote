72ccf536eb29272cfa2fbfa5cd5bdace8f4be72f
greyw0rks <elchapidave@gmail.com>
2026-05-24 15:32:52 +0100

    docs(tests): batch 15 coverage note 6 of 3

--- tests/clarityvote.test.ts
+++ tests/clarityvote.test.ts
@@ -117,3 +117,5 @@ describe("clarityvote", () => {
 // batch-14-coverage-7: additional test coverage target
 
 // batch-14-coverage-8: additional test coverage target
+
+// batch-15-coverage-6: additional test coverage target
